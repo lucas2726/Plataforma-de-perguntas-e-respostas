bc57d948d04d22ec48b97bb6c0fc24f8ca068290
LucasBueno2726 <140419660+lucas2726@users.noreply.github.com>
2023-10-31 11:10:48 -0400

    ex

---
+++ database/database.js
@@ -0,0 +1,10 @@
+//Para exportar a biblioteca
+const sequelise = require('sequelize')
+
+//Para fazer a conexão com o banco de dados
+const connection = new sequelize('guiaperguntas', 'root', 'root', /*Nome do banco de dados, usúario e senha */ {
+    host: 'localhost', //Onde está rodando,
+    dialect: 'mysql' //Nome de qual banco de dados está rodando
+})
+
+module.exports = connection
--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require("express")
 const app = express()
+const bodyParser = require("body-parser")
+const connection = require("./database/database")
+//database
 
+connection
+      .authenticate()
+      .then(() => {
+          console.log("Conexão feita com o banco de dados")
+      })
+      .catch((msgErro) => {
+        console.log("ERRO!")
+      })
+//Esstou dizendo para o express usar o EJS como view engine
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-
+//Body parser
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+//Rotas
 app.get("/", (req, res) => {
     res.render("index")
 })
@@ -13,10 +28,13 @@ app.get("/perguntar", (req, res) => {
 })
 
 app.post("/salvarpergunta", (req, res) => {
-    res.send("recebido")
+    let titulo = req.body.titulo
+    let descricao = req.body.descricao
+    res.send("recebido" + titulo + " " + descricao)
 })
 
 app.listen(8080, () => {console.log("App rodando!")})
+
 
 
 
