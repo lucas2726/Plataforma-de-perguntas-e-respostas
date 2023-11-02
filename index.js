9f89e756ba70f901ff16810c8d076ab58dc45695
LucasBueno2726 <140419660+lucas2726@users.noreply.github.com>
2023-11-02 13:40:47 -0400

    ex

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require("express")
-const app = express()
-const bodyParser = require("body-parser")
-const connection = require("./database/database") //Para importar a biblioteca
-const Pergunta = require("./database/Pergunta") //Para importar o banco de dados js
-const Resposta = require("./database/Resposta") //Para importar a resposta 
+const express = require("express") //Para chamar o express instalado
+const app = express() //Para inicializar o express
+const bodyParser = require("body-parser") //Para conseguir acessar os elementos html, pelo req.body
+const connection = require("./database/database") //Para importar o database
+const Pergunta = require("./database/Pergunta") //Para importar o banco de dados Pergunta
+const Resposta = require("./database/Resposta") //Para importar o banco de dados Resposta
 //database
 connection
       .authenticate()
@@ -17,26 +17,27 @@ connection
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 //Body parser
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false})) //Para analisar os dados do html e só aceitar array e string
+app.use(bodyParser.json()) //Para analisar os dados do html e tronar-los disponiveis no req.body
 //Rotas
 app.get("/", (req, res) => {
-    Pergunta.findAll({raw:true, order:[/*ASC crescente / DESK decrescente / Raw é para fazer um pesquisa mais limpa*/
+    Pergunta.findAll({raw:true, order:[/*findAll: Serve para fazer uma pesquisa no mySQL.ASC crescente / DESK decrescente / Raw é para fazer um pesquisa mais limpa e order é para colocar na order desejada*/
         ['id','DESC']
-    ]}).then(perguntas => { //Para buscar dados. 
-    res.render("index", {
+    ]}).then(perguntas => {  
+    res.render("index", { //Res.render serve para enviar uma resposta para o html
         perguntas: perguntas
     })
     })
 })
+
 
 app.get("/perguntar", (req, res) => {
     res.render("perguntar")
 })
 
 app.post("/salvarpergunta", (req, res) => {
-    let titulo = req.body.titulo
-    let descricao = req.body.descricao
+    let titulo = req.body.titulo //Para traser para o node o name do html
+    let descricao = req.body.descricao //Para traser para o node o name do html
     Pergunta.create({ //Pata conectar com a const Pergunta que está conectada com o BD
         titulo: titulo, //Para conectar com o banco de dados, nesse caso: Pergunta
         descricao: descricao //Para conectar com o banco de dados, nesse caso: Pergunta
@@ -52,7 +53,7 @@ app.get("/pergunta/:id",(req,res) => {
     }).then(pergunta => {
         if (pergunta != undefined) { //encontrada
           Resposta.findAll({
-            where:{perguntaId: pergunta.id},
+            where:{perguntaId: pergunta.id}, //Para dizer oque vc quer buscar no mySQL
             order:[['id', 'DESC']]
           }).then(respostas => {
             res.render("pergunta", { //Para pegar no html as respostas
@@ -69,7 +70,7 @@ app.get("/pergunta/:id",(req,res) => {
 app.post("/responder", (req, res) => {
     let corpo = req.body.corpo//Para conectar com o html
     let perguntaId = req.body.pergunta//Para conectar com o html
-    Resposta.create({
+    Resposta.create({ //Serve para cria os dados no bd
         corpo: corpo,//Para conectar com o banco de dados, nesse caso: Resposta
         perguntaId: perguntaId //Para conectar com o banco de dados, nesse caso: Resposta
     }).then(() => {
